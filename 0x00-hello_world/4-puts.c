79c3888226ff49cd4f32adeab85ebb378a1eb164
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-03-16 11:21:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stadio.h>
+/**
+ * main - A program that prints aline  
+ *
+ * return alwyes 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
