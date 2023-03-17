5adff80abacb2262007c12f55b825bd759805507
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-03-17 14:27:14 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,12 +14,12 @@ int main(void)
 	c = 'a';
 	d = '0';
 	while
-		(d < '10') {
-			putchar(d+10);
+		(d < 10) {
+			putchar(d+'0');
 			d++;
 		}
 	while
-		(c <= 'a') {
+		(c <= 'f') {
 			putchar(c);
 			c++;
 		}
