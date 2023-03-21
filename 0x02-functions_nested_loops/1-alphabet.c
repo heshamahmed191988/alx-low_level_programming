1a9f7aaa2f49d2842ae0911b1d7772e3f81a4a29
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-03-21 03:07:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char n;
+n = 'a';
+while (n <= 'z')
+{
+putchar (n);
+n++;
+}
+return (0);
+}
