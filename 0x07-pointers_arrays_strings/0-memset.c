fa624455722ebc5cb8bab006c926eb7fb8090930
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-04-03 07:38:12 -0700

    l

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_memset - fill a block of memory with a specific value
+ * make ablock of memory with spsific value
+ * @s: the address of memory to print
+ * @b: value
+ * @n: number of bytes
+ *
+ * Return: s.
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+int z = 0;
+for (n > 0; z++)
+{
+s[i] = b;
+n--;
+}
+return (s);
+}
