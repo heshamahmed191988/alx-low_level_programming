083759b40f58a328f0103942eae1437b9a75ba9a
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-04-03 10:16:00 -0700

    l

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+ #include "main.h"
+/**
+ * _strchr is the entry point 
+ * @s is a value
+ * @c is a value too
+ * return : alwayes 0
+ */
+char *_strchr(char *s, char c)
+{
+	int y = 0;
+
+	for (; s[y] >= '\0'; y++ )
+	{
+		if (s[y] ==)
+			return (&s[i]);
+	}
+	return (0);
+}
