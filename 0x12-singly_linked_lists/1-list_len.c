306b357de4dee1e654e935085e2ca331480fa044
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-04-27 14:30:23 -0700

    l

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t l = 0;
+	size_t n = 0;
 
-	while (z)
+	while (h)
 	{
-		l++;
-		z = z->next;
+		n++;
+		h = h->next;
 	}
-	return (l);
+	return (n);
 }
